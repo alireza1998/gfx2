26984001b052dffdf3eee18081dd8d742f79cbf4
alireza1998 <alireza.eidani1000@gmail.com>
2016-01-20 06:20:54 +0330

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,21 @@
+do
+
+function run(msg, matches)
+  return 'Umbrella Telegram Bot v1.5'.. [[ 
+  
+  Antispam Bot: ✌️🏻 》GFX《 ✌️🏻
+  Channel: @GFX_SUPPORT
+  Sudo (Admin): @alireza_PT
+  
+  Powered by:
+  《manufacturer 》<GFX>
+end
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^ver$"
+  }, 
+  run = run 
+}
+end
